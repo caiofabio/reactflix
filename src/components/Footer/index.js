e7375cf8249eea0b9e459c7a6dc8a87cbfd64abb
Caio Fabio Ramos <caiofabiocf@gmail.com>
2020-07-30 13:48:09 -0300

    add form, alt footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { FooterBase } from './styles';
-
+import Logo from '../../assets/img/alurav2.png'
+//<img src="https://www.alura.com.br/assets/img/imersoes/aula/logo-alura-header.1595847989.svg" alt="Logo Alura" />
 function Footer() {
   return (
     <FooterBase>
       <a href="https://www.alura.com.br/">
-        <img src="https://www.alura.com.br/assets/img/alura-logo-white.1570550707.svg" alt="Logo Alura" />
+        
+        <img className="Logo" src={Logo} alt="PIKACHU logo"></img>
       </a>
       <p>
         Orgulhosamente criado durante a
